5722bf8bc02817c482154b1b1e219fe0b6c07f59
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2021-11-11 10:55:39 +0900

    検索機能
    検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,22 @@ Rails.application.routes.draw do
   get 'places/index'
   get 'places/search'
   get 'plase/search'
+  post 'cooks/new'
+      
   devise_for :users
   root to: "places#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cooks do
     collection do
       get 'search'
+    
       
       
     end
   
 
     resources :places, only: [:index,:new,:create]
-    resources :cook, only: [:index,:new,:create]
+    resources :cooks, only: [:index,:new,:create]
     
   end
 end
