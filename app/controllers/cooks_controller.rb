939b6a2c163cc67beb1dbe4dacc7c5878570829b
yusukexyusuke <yusukexyusuke@gmail.com>
2021-12-08 00:46:03 +0900

    work

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,40 +1,34 @@
 class CooksController < ApplicationController
   before_action :search_product, only: [:index, :search]
 
- 
   def index
     @cooks = Cook.all
   end
-  
+
   def new
     @cook = Cook.new
   end
-  
+
   def create
     @cook = Cook.new(cooks_params)
-   if @cook.save
-     redirect_to  root_path
-   else
-     render :new
-   end
- end
+    if @cook.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def search
     @results = @p.result.includes(:category)
   end
- 
- 
 
- private
+  private
+
   def cooks_params
-    params.require(:cook).permit(:image,:title,:store,:cooksentence)
-
-
+    params.require(:cook).permit(:image, :title, :store, :cooksentence)
   end
 
- private
-
- def search_cook
-  @p = cook.ransack(params[:q])
- end
+  def search_cook
+    @p = cook.ransack(params[:q])
+  end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,40 +1,32 @@
 class PlacesController < ApplicationController
-
   before_action :search_place, only: [:index, :search]
 
- 
-  
   def index
-    @places = Place.all 
-   
-
+    @places = Place.all
   end
 
   def new
     @cook = Cook.new
   end
-  
+
   def create
     @cook = Cook.new(cooks_params)
-   if @cook.save
-     redirect_to  root_path
-   else
-     render :new
-   end
-  end
-    def search
-      @results = @p.result.includes(:category)
-      @places = Place.all 
-      @cooks = Cook.all
-
+    if @cook.save
+      redirect_to root_path
+    else
+      render :new
     end
-   
-    
+  end
+
+  def search
+    @results = @p.result.includes(:category)
+    @places = Place.all
+    @cooks = Cook.all
+  end
+
   private
 
   def search_place
-    @p = Place.ransack(params[:q])  
+    @p = Place.ransack(params[:q])
   end
-
-  
 end
